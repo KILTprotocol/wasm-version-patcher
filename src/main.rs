0b6e0162ff9be4a539f2b318ffde2152fa1f3545
weichweich <albrecht@kilt.io>
2022-10-06 09:25:07 +0200

    feat: version from file with RON

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,9 @@
 use clap::Parser;
 use sc_executor::read_embedded_version;
 use sc_executor_common::runtime_blob::RuntimeBlob;
-use sp_version::{create_runtime_str, RuntimeVersion};
+use sp_version::RuntimeVersion;
 use std::fs::File;
 use std::io::prelude::*;
-
-#[sp_version::runtime_version]
-pub const VERSION: RuntimeVersion = RuntimeVersion {
-    spec_name: create_runtime_str!("kilt-spiritnet"),
-    impl_name: create_runtime_str!("kilt-spiritnet"),
-    authoring_version: 1,
-    spec_version: 10730,
-    impl_version: 0,
-    apis: sp_version::create_apis_vec!([
-        ([223u8, 106, 203, 104, 153, 7, 96, 155], 3u32),
-        ([55u8, 227, 151, 252, 124, 145, 245, 228], 1u32),
-        ([188u8, 157, 137, 144, 79, 91, 146, 63], 1u32),
-        ([55u8, 200, 187, 19, 80, 169, 162, 168], 1u32),
-        ([64u8, 254, 58, 212, 1, 248, 149, 154], 1u32),
-        ([210u8, 188, 152, 151, 238, 208, 143, 21], 1u32),
-        ([247u8, 139, 39, 139, 229, 63, 69, 76], 1u32),
-        ([171u8, 60, 5, 114, 41, 31, 235, 139], 1u32),
-        ([221u8, 113, 141, 92, 197, 50, 98, 212], 1u32),
-        ([234u8, 147, 227, 241, 111, 61, 105, 98], 1u32)
-    ]),
-    transaction_version: 1,
-    state_version: 0,
-};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -40,6 +17,11 @@ struct Args {
     #[arg(short, long)]
     output: String,
 
+    /// Path to version file
+    #[arg(short = 'r', long)]
+    replacement: String,
+
+    /// Write as hex
     #[arg(short = 'x', long, default_value = "false")]
     hex: bool,
 }
@@ -47,46 +29,62 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    let mut file = File::open(args.input).expect("Failed to open input file");
-    let mut contents = Vec::new();
-    file.read_to_end(&mut contents)
+    let mut wasm_in_file = File::open(args.input).expect("Failed to open input file");
+    let mut wasm_input = Vec::new();
+    wasm_in_file
+        .read_to_end(&mut wasm_input)
         .expect("Failed to read file");
 
+    let mut version_in_file = File::open(args.replacement).expect("Failed to open input file");
+    let mut version_input = String::new();
+    version_in_file
+        .read_to_string(&mut version_input)
+        .expect("Failed to read file");
+
+    // try decode hex if it starts with 0x
+    if &wasm_input[..2] == b"0x" {
+        wasm_input = hex::decode(&wasm_input[2..]).unwrap();
+    }
+
+    // try decompress
     let wasm = sp_maybe_compressed_blob::decompress(
-        &contents[..],
+        &wasm_input[..],
         sp_maybe_compressed_blob::CODE_BLOB_BOMB_LIMIT,
     )
-    .expect("WASM should decompress");
+    .expect("should be valid wasm");
 
-    let old_blob = RuntimeBlob::new(&wasm).expect(
-        "Embedded old blob is
-	valid",
-    );
+    let old_blob = RuntimeBlob::new(&wasm).expect("Embedded old blob is invalid");
     println!(
-        "OLD spec version is {:#?}",
-        read_embedded_version(&old_blob)
-            .ok()
-            .flatten()
-            .expect("Reading embedded version works")
+        "OLD spec version is {}",
+        serde_json::to_string(
+            &read_embedded_version(&old_blob)
+                .ok()
+                .flatten()
+                .expect("Reading embedded version should work")
+        )
+        .expect("should serialize")
     );
 
-    let fixed_wasm = sp_version::embed::embed_runtime_version(&wasm[..], VERSION)
+    let version: RuntimeVersion =
+        ron::from_str(&version_input).expect("RuntimeVersion should be valid");
+
+    let fixed_wasm = sp_version::embed::embed_runtime_version(&wasm[..], version)
         .expect("Failed to write version");
 
-    let new_blob = RuntimeBlob::new(&fixed_wasm).expect(
-        "Embedded old blob is
-        valid",
-    );
+    let new_blob = RuntimeBlob::new(&fixed_wasm).expect("Embedded old blob is invalid");
     println!(
-        "NEW spec version is {:#?}",
-        read_embedded_version(&new_blob)
-            .ok()
-            .flatten()
-            .expect("Reading embedded version works")
+        "NEW spec version is {:?}",
+        serde_json::to_string(
+            &read_embedded_version(&new_blob)
+                .ok()
+                .flatten()
+                .expect("Reading new embedded version failed")
+        )
+        .unwrap()
     );
 
     let new_blob_comp = sp_maybe_compressed_blob::compress(
-        &fixed_wasm[..],
+        &new_blob.serialize()[..],
         sp_maybe_compressed_blob::CODE_BLOB_BOMB_LIMIT,
     )
     .expect("WASM should decompress");
